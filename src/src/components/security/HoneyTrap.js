58e521ad1bae1c4a3d2f8d9cd88e531aa002e669
irsadraja48-design <irsadraja48@gmail.com>
2026-07-06 21:41:24 +0530

    Update HoneyTrap.js

--- src/src/components/security/HoneyTrap.js
+++ src/src/components/security/HoneyTrap.js
@@ -1,14 +1,9 @@
-
-// HoneyTrap.js
 import React from 'react';
 
-const HoneyTrap = () => {
-  return (
-    <div style={{ opacity: 0, position: 'absolute', top: -9999 }}>
-      <label htmlFor="website">Leave this field empty:</label>
-      <input type="text" id="website" name="website" tabIndex="-1" autoComplete="off" />
-    </div>
-  );
-};
+const HoneyTrap = () => (
+  <div style={{ display: 'none' }}>
+    <input type="text" id="website" name="website" tabIndex="-1" autoComplete="off" />
+  </div>
+);
 
 export default HoneyTrap;
