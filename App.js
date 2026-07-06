2291a057850ebe5f4f86e4f4d25949a59e2d2d3d
irsadraja48-design <irsadraja48@gmail.com>
2026-07-06 21:24:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+import Login from './src/src/pages/Login'; // आपके फोल्डर स्ट्रक्चर के अनुसार पाथ
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
