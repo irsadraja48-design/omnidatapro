be370e73a71a1f933e9ff98f4ed7d931577ccced
irsadraja48-design <irsadraja48@gmail.com>
2026-07-07 21:17:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Login successful!');
-});
+function showScreen(screenId) {
+    document.getElementById('login-box').style.display = 'none';
+    document.getElementById('signup-box').style.display = 'none';
+    document.getElementById('forget-box').style.display = 'none';
+    document.getElementById(screenId).style.display = 'block';
+}
